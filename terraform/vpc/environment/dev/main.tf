b4f10f0d98502d5aea191912148a3fc62af10ddc
Peter Penido de Souza <114949863+peterpenido@users.noreply.github.com>
2023-08-24 09:27:55 -0300

    Update main.tf

--- terraform/vpc/environment/dev/main.tf
+++ terraform/vpc/environment/dev/main.tf
@@ -8,9 +8,9 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "cloudgeeks-backend1"
-    key = "cloudgeeks.tfstate"
-    region = "us-east-1"
+    bucket = "petterpenidotf"
+    key = "stg/tfdev.tfstate"
+    region = "us-east-2"
   }
 }
 
